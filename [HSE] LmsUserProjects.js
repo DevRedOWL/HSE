41ab44c8de7cbffb3e8c0e90b548b3317f7bf8b2
Daniil <vda.perm@mail.ru>
2019-04-05 04:27:11 +0500

    Create [HSE] LmsUserProjects.js

---
+++ [HSE] LmsUserProjects.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         [HSE] LmsUserProjects_Min
+// @namespace    https://hse.devredowl.ru/customs
+// @version      1.0 beta
+// @description  User Own LMS
+// @author       Daniel Varentsov
+// @match        https://lms.hse.ru/student.php?ctg=personal*
+// @require      http://devredowl.ru/PublicJS/jq330.js
+// @require      http://devredowl.ru/PublicJS/crypto.js
+// @require      http://devredowl.ru/PublicJS/sweetalert.js
+// @require      https://raw.githubusercontent.com/DevRedOWL/HSE/master/lms_up.min.js
+// @grant        none
+// ==/UserScript==
